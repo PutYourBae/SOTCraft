8fdae8c5c5b3b0766b755fc02cc4dd027a974f8e
PutYourBae <agustianputra2108@gmail.com>
2026-05-22 02:23:50 +0700

    feat: add year to formatted date in history display

--- src/App.tsx
+++ src/App.tsx
@@ -145,6 +145,7 @@ function formatDate(value: string) {
   return new Intl.DateTimeFormat('id-ID', {
     day: '2-digit',
     month: 'short',
+    year: 'numeric',
     hour: '2-digit',
     minute: '2-digit',
   }).format(new Date(value))
